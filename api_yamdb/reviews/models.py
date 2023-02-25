add09b50d5c097a87766d531ef303a44ecb14590
Max Trukhinov <93321964+StriderDunedain@users.noreply.github.com>
2023-02-26 00:02:42 +0300

    Update models.py

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -135,7 +135,7 @@ class Review(models.Model):
     author = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name='reviews'
     )
-    score = models.IntegerField(choices=Rating.choices)
+    rating = models.IntegerField(choices=Rating.choices)
     pub_date = models.DateTimeField(auto_now_add=True)
     title = models.ForeignKey(
         Title, on_delete=models.CASCADE, related_name='titles'
